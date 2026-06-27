9a1ffc2710f36945d30be72dd00e347ddf9aec4e
Abdul Rehman <dragonbhatti@gmail.com>
2026-06-27 14:08:02 +0500

    Created 2 nodes in state graph, one for planning through user prompt, one for architecturing through task plan

---
+++ agent/graph.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+from langchain_groq.chat_models import ChatGroq
+from langgraph.constants import END
+from langgraph.graph import StateGraph
+
+from agent.prompts import *
+from agent.states import *
+
+load_dotenv()
+
+
+llm = ChatGroq(model="openai/gpt-oss-120b")
+
+def planner_agent(state: dict) -> dict:
+    """Converts user prompt into a structured Plan."""
+    user_prompt = state["user_prompt"]
+    resp = llm.with_structured_output(Plan).invoke(
+        planner_prompt(user_prompt)
+    )
+    if resp is None:
+        raise ValueError("Planner did not return a valid response.")
+    return {"plan": resp}
+
+
+def architect_agent(state: dict) -> dict:
+    """Creates TaskPlan from Plan."""
+    plan: Plan = state["plan"]
+    resp = llm.with_structured_output(TaskPlan).invoke(
+        architect_prompt(plan=plan.model_dump_json())
+    )
+    if resp is None:
+        raise ValueError("Planner did not return a valid response.")
+
+    resp.plan = plan
+    print(resp.model_dump_json())
+    return {"task_plan": resp}
